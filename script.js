c223e310ad9ec948344eabe5675794be0533c86b
Aravindh Siva <adamaravindh@gmail.com>
2019-06-09 12:47:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-    var last;
+    var last,temp;
     function loader(){
         let req = new XMLHttpRequest();
         req.onreadystatechange = () => {
         if (req.readyState == XMLHttpRequest.DONE) {
-            last = req.responseText;
-            this.show(JSON.parse(last).data);
+            temp = req.responseText;
+            last = JSON.parse(req.responseText).data;
+            this.show(last);
             }
         };
 
@@ -14,29 +15,46 @@
         req.send();
     }
     function apiCall(){
-        console.log("Last in Call :",last);
         let req = new XMLHttpRequest();
         req.open("PUT", "https://api.jsonbin.io/s/5cfc124958196b429f50ad8d", true);
         req.setRequestHeader("Content-type", "application/json");
         req.setRequestHeader("secret-key","$2a$10$T4A0vojC9ffvrUo9t9B44euZmspAa3x57VvnkYNHoGrkb8SafuTru");
-        var myObj=last;
+        var myObj=temp;
         req.onreadystatechange = () => {
         if (req.readyState == XMLHttpRequest.DONE) {
-            var resp = JSON.parse(req.responseText);
-            console.log(resp);
-            temp = resp.data;
+            var resp = JSON.parse(req.responseText);        
             location.reload();
             }
         };
         var jsonObj = JSON.parse(myObj);
-        console.log(jsonObj);
         if(document.getElementById('inputData').value !== "")
             jsonObj.data.push(document.getElementById('inputData').value);
-        console.log(jsonObj);
         req.send(JSON.stringify(jsonObj));
     }
     function show(data){
         for(var i=0;i<data.length;i++){
-            document.getElementById('show').innerHTML += "<p class='card cards'>"+data[i]+"</p>"
+            document.getElementById('show').innerHTML += "<span class='card cards'>"+data[i]+"<i class='fas fa-times' onclick='remove(event)'></i></span><br>"
         }
+    }
+    function remove(e){
+        let s = e.target.parentElement.innerText;
+        this.last = this.find(this.last,s);
+        let req = new XMLHttpRequest();
+
+        req.onreadystatechange = () => {
+        if (req.readyState == XMLHttpRequest.DONE) {
+            console.log(req.responseText);
+            }
+        };
+
+        req.open("PUT", "https://api.jsonbin.io/s/5cfc124958196b429f50ad8d", true);
+        req.setRequestHeader("Content-type", "application/json");
+        req.setRequestHeader("secret-key", "$2a$10$T4A0vojC9ffvrUo9t9B44euZmspAa3x57VvnkYNHoGrkb8SafuTru");
+        req.send(JSON.stringify({"data":this.last}));
+        location.reload();
+    }
+    function find(array, element) {
+        const index = array.indexOf(element);
+        array.splice(index, 1);
+        return array;
     }
